06f95232cba498a996e512b538f789e9530ae4a4
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-01-24 16:33:10 +0530

    UI Changed

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-<h1 align="center">Toss The Game</h1>
-
-This is a Simple Javascript Game in which you have to reload the website to Toss the Dice. 
+<h1 align="center">Toss The Dice</h1>
+<h2 align="center">Simple JavaScript Game</h2>
--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,27 @@
-// Photos Change karniya ne 
-// Heading Change Karni hai
-// random number
-
 function randomNumber() {
     var num = Math.random();
-    num = Math.floor(num * 6) + 1  
-    return num; 
+    num = Math.floor(num * 6) + 1
+    return num;
 }
 
+function tossthedice() {
 
+    var player1 = randomNumber();
+    var player2 = randomNumber();
 
+    var playerImg1 = "assets/images/" + "dice" + player1 + ".png";
+    var playerImg2 = "assets/images/" + "dice" + player2 + ".png";
 
-var player1 = randomNumber();
-var player2 = randomNumber();
+    document.querySelector("#Player1 img").setAttribute("src", playerImg1);
+    document.querySelector("#Player2 img").setAttribute("src", playerImg2);
 
-var playerImg1 = "assets/images/"+"dice"+player1+".png";
-var playerImg2 = "assets/images/"+"dice"+player2+".png";
-
-console.log(playerImg1);
-console.log(playerImg2);
-document.querySelector("#Player1 img").setAttribute("src", playerImg1);
-document.querySelector("#Player2 img").setAttribute("src", playerImg2);
-
-if(player1 > player2){
-    document.querySelector("h1").innerText = "🏆 Player 1 Wins"
+    if (player1 > player2) {
+        document.querySelector("h1").innerText = "🏆 Player 1 Wins"
+    }
+    else if (player1 < player2) {
+        document.querySelector("h1").innerText = "Player 2 Wins 🏆"
+    }
+    else if (player1 == player2) {
+        document.querySelector("h1").innerText = "Toss Again 🎲"
+    }
 }
-else if(player1 < player2){
-    document.querySelector("h1").innerText = "Player 2 Wins 🏆"
-}
-else if(player1 == player2){
-    document.querySelector("h1").innerText = "Refresh Me"
-}
-
-
-
-
